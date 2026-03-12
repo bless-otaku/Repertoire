0fff7a453c655afce559c689c39fcd721acc6035
usbbush <jeanbienvenusb@gmail.com>
2026-03-12 13:53:59 +0200

    Sauvergarder le contact

--- Repertoire/Form1.cs
+++ Repertoire/Form1.cs
@@ -14,20 +14,30 @@ namespace Repertoire
         {
             if (string.IsNullOrEmpty(txtName.Text) || string.IsNullOrEmpty(txtPhone.Text))
             {
+                // Valider que les champs ne sont pas vides et ont une valeur valide
                 labelMsg.Text = "l'un de champ est vide";
-                labelMsg.ForeColor = Color.Red;
+                labelMsg.ForeColor = Color.Blue;
             }
             else
             {
                 labelMsg.Text = "";
-                string path = "contact.txt";
+                // Nous allons enregistrer les donnees dans un fichier csv
+                string path = "contact.csv";
+                // Verifier si le fichier existe ou pas
                 if (! File.Exists(path)) // ca verifie si le fichier n'existe pas 
                 {
                     // Alors nous creons un nouveau fichier
+                    using (StreamWriter sw = new StreamWriter(path))
+                    {
+                        sw.WriteLine("Name,Phone"); // ecrire les en-tetes du fichier csv
+                        sw.WriteLine($"{txtName.Text},{txtPhone.Text}"); // ecrire les donnees dans le fichier csv
+                    }
                 } else
-                {
-                    // ajouter le nouvelles donnees aux fichier 
-                }
+                {                     // ajouter le nouvelles donnees aux fichier 
+                    using (StreamWriter sw = new StreamWriter(path, true)) // le parametre true permet d'ajouter les nouvelles donnees a la fin du fichier au lieu de remplacer les anciennes donnees
+                    {
+                        sw.WriteLine($"{txtName.Text},{txtPhone.Text}"); // ecrire les donnees dans le fichier csv
+                    }
             }
 
         }
